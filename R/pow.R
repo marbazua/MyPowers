a6cf26fb7835f3d341c13b8acb4f21fd182eb17a
Mar <maria.lobato@stat.ubc.ca>
2017-11-26 19:13:52 -0800

    Commit 2

--- R/pow.R
+++ R/pow.R
@@ -1,12 +1,12 @@
 #' Generic Power Function
 #'
-#' This function raises a vector to any power. I'm making it internal,
-#' by not \code{export}ing, because I don't want users to use it.
-#' Why not? Because I said so!
+#' This is an intern function.
+#' This function raises a vector to any power.
+#' This funtion also plots the raised powers.
 #'
 #' @param x Vector to raise to some power.
 #' @param a Power to raise \code{x} by.
-#' @param add_plot Display a plot of \code{x} vs the output? Use logical.
+#' @param add_plot Display a plot of \code{x} vs the output. Use logical.
 #'
 #' @return The vector \code{x}, raised to the power of \code{a}.
 
--- R/powers-package.r
+++ R/powers-package.r
@@ -3,6 +3,8 @@
 #' With this package you can apply powers to your vectors by
 #' using functions
 #'
+#' You can also find the solution of a second degree polynomial
+#'
 #' @name powers
 #' @author Mar Bazua
 #' @note This package is only for class assignment purposes
