c46bf414d5332cbd178c94bb312924b2230a05b4
Mar <maria.lobato@stat.ubc.ca>
2017-11-28 19:48:19 -0800

    commit4

--- R/box_cox.R
+++ R/box_cox.R
@@ -5,6 +5,9 @@
 #' @param x Vector to apply the Box-Cox to
 #' @param l The parameter lambda
 #'
+#' @param add_plot If a plot with the transformed vector should be plotted.
+#' (The default is FALSE)
+#'
 #' @return The vector of the transformed \code{x}.
 #' @details
 #' \itemize{
@@ -18,11 +21,20 @@
 #'
 #' @export
 #'
-box_cox<- function(x, l){
+box_cox<- function(x, l, add_plot=FALSE){
   x<-x[!is.na(x)]
   if ((any(x<=0))) {stop("All the entries of x must be positive")}
-  if (l==0){return(log(x))}
-  return((pow(x,l,FALSE)-1)/l)
+  if (l==0){
+    if (add_plot==TRUE){
+    print(ggplot2::qplot(x, log(x)))
+    }
+    return(log(x))
+  }
+  res2<-(pow(x,l,FALSE)-1)/l
+  if (add_plot==TRUE){
+    print(ggplot2::qplot(x, res2))
+  }
+  return(res2)
 }
 
 
--- R/powers-package.r
+++ R/powers-package.r
@@ -1,10 +1,13 @@
 #' Computation of Powers
 #'
-#' With this package you can apply powers to your vectors by
-#' using functions
-#'
-#' You can also find the solution of a second degree polynomial
-#'
+#' With this package you can
+#' \itemize{
+#' \item Square a vector \code{square(x,...)}
+#' \item Cube a vector \code{cube(x,..)}
+#' \item Obtain the reciprocal of a vector \code{reciprocal(x,...)}
+#' \item Apply a Box-Cox transformation to a vector \code{box_cox(x,l,..)}
+#' \item Find the solution of a second degree polynomial \code{polynom(a,b,c)}
+#' }
 #' @name powers
 #' @author Mar Bazua
 #' @note This package is only for class assignment purposes
